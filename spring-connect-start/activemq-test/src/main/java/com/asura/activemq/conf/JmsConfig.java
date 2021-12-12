479b5f497b7ef73d84b237a6374637cdf01946d3
asura <77570648@qq.com>
2021-12-13 00:39:31 +0800

    activemq and redis update

--- spring-connect-start/activemq-test/src/main/java/com/asura/activemq/conf/JmsConfig.java
+++ spring-connect-start/activemq-test/src/main/java/com/asura/activemq/conf/JmsConfig.java
@@ -5,8 +5,11 @@ import org.apache.activemq.command.ActiveMQQueue;
 import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.stereotype.Component;
 
+import javax.jms.ConnectionFactory;
 import javax.jms.Queue;
 import javax.jms.Topic;
 
@@ -26,5 +29,17 @@ public class JmsConfig {
     @Bean
     public Topic topic(){
         return new ActiveMQTopic(topicName);
+    }
+
+
+    /**
+     * JmsListener注解默认只接收queue消息,如果要接收topic消息,需要设置containerFactory
+     */
+    @Bean
+    public JmsListenerContainerFactory<?> topicListenerContainer(ConnectionFactory activeMQConnectionFactory) {
+        DefaultJmsListenerContainerFactory topicListenerContainer = new DefaultJmsListenerContainerFactory();
+        topicListenerContainer.setPubSubDomain(true);
+        topicListenerContainer.setConnectionFactory(activeMQConnectionFactory);
+        return topicListenerContainer;
     }
 }
--- spring-connect-start/activemq-test/src/main/java/com/asura/activemq/dao/MessageTemplate.java
+++ spring-connect-start/activemq-test/src/main/java/com/asura/activemq/dao/MessageTemplate.java
@@ -1,12 +1,9 @@
 package com.asura.activemq.dao;
 
-import com.asura.activemq.conf.JmsConfig;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsMessagingTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.jms.JMSException;
@@ -41,7 +38,12 @@ public class MessageTemplate {
     }
 
     @JmsListener(destination = "${queue.activemq}")
-    public void received(TextMessage textMessage) throws JMSException {
-        System.out.println("********消费者收到消息：" + textMessage.getText());
+    public void received1(TextMessage textMessage) throws JMSException {
+        System.out.println("********消费者收到queue消息：" + textMessage.getText());
+    }
+
+    @JmsListener(destination = "${queue.topicName}", containerFactory="topicListenerContainer")
+    public void received2(TextMessage textMessage) throws JMSException {
+        System.out.println("********消费者收到topic消息：" + textMessage.getText());
     }
 }

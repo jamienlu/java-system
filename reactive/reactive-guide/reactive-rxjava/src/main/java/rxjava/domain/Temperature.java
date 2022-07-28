967e9cbc353b7accb8b65c3f8b43b31519f87bfe
77570648@qq.com <77570648@qq.com>
2022-07-28 21:15:18 +0800

    clean

--- reactive/reactive-guide/reactive-rxjava/src/main/java/rxjava/domain/Temperature.java
+++
@@ -1,8 +0,0 @@
-package rxjava.domain;
-
-import lombok.Data;
-
-@Data
-public class Temperature {
-	private final double value;
-}
--- reactive/reactive-guide/reactive-springevent/src/main/java/springevent/domain/Temperature.java
+++
@@ -1,8 +0,0 @@
-package springevent.domain;
-
-import lombok.Data;
-
-@Data
-public class Temperature {
-	private final double value;
-}
